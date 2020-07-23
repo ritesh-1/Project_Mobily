038f9aefe9091658f0621dbb6b310b381adb8757
ritesh-1 <68611327+ritesh-1@users.noreply.github.com>
2020-07-23 17:48:53 +0530

    Update Cart.php
    Cart update

--- Cart.php
+++ Cart.php
@@ -7,11 +7,11 @@
 <?php
 
     //*  include cart items if it is not empty */
-    count($product->getData('cart')) ? include ('Template/_cart-template.php') :  include ('Template/notFound/_cart_notFound.php');
+    count($product->getData('cart')) ? include ('Template/_carts.php') :  include ('Template/notFound/_cart_notFound.php');
     /*  include cart items if it is not empty */
 
     /*  include top sale section */
-    count($product->getData('wishlist')) ? include ('Template/_wishilist_template.php') :  include ('Template/notFound/_wishlist_notFound.php');
+    count($product->getData('wishlist')) ? include ('Template/_wishlist_template.php') :  include ('Template/notFound/_wishlist_notFound.php');
     /*  include top sale section */
 
     //include new phones
